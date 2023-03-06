a2234d2b4f3d68205227dd968bd6e477b43fb0cb
mattcmoore <matt@mattcmoore.com>
2023-03-06 10:15:01 -0800

    clean up index.js

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './navbar.css';
-<<<<<<< react_app/src/index.js
-=======
 import 'tippy.js/dist/tippy.css';
->>>>>>> react_app/src/index.js
 import App from './App';
 
 
