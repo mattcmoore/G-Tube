ee01fe1df16a17f7dedff65f08c73b2500f88cfd
mattcmoore <matt@mattcmoore.com>
2023-03-06 09:49:44 -0800

    cleanup commit

--- react_app/src/components/Secondary/Dismissible.jsx
+++ react_app/src/components/Secondary/Dismissible.jsx
@@ -1,15 +1,28 @@
 import Thumbnail from "./Thumbnail.jsx"
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {DateTime} from 'luxon'
 
 
 
 const Dismissible = ({video_link,username,title,views,date_published,runtime}) => {
-   // I will need fetched video data as a prop here
+
+    const [isVisible, setIsVisible] = useState(true)
+
+    useEffect(() => {
+        setIsVisible(true)
+    },[])
+
+    const handleMouseEnter = () => {
+        // setIsVisible(true)
+    }
+
+    const handleMouseLeave = () => {
+        // setIsVisible(false)
+    }
 
    return(
         <div className="dismissible">
-            <Thumbnail video_link={video_link} runtime={runtime}  />
+            <Thumbnail video_link={video_link} runtime={runtime} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave()}  />
             <div className="details">
                 <p className="video-title">{title}</p>
                 <div className="user_name">{username}</div>
@@ -19,6 +32,18 @@ const Dismissible = ({video_link,username,title,views,date_published,runtime}) =
                     <p>{DateTime.fromISO(date_published).toRelative()}</p>
                 </div>    
             </div>
+            <button className={isVisible ? "queue-btn" : "hidden"}>
+                <svg
+                    className="three-dots" 
+                    viewBox="0 0 24 24" 
+                    preserveAspectRatio="xMidYMid meet" 
+                    focusable="false"
+                >    
+                    <g>
+                        <path d="M12,16.5c0.83,0,1.5,0.67,1.5,1.5s-0.67,1.5-1.5,1.5s-1.5-0.67-1.5-1.5S11.17,16.5,12,16.5z M10.5,12 c0,0.83,0.67,1.5,1.5,1.5s1.5-0.67,1.5-1.5s-0.67-1.5-1.5-1.5S10.5,11.17,10.5,12z M10.5,6c0,0.83,0.67,1.5,1.5,1.5 s1.5-0.67,1.5-1.5S12.83,4.5,12,4.5S10.5,5.17,10.5,6z"></path>
+                    </g>
+                </svg>
+            </button>
         </div>
     )
 }
