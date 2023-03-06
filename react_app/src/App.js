6922beef24e1e5c1eb885a6fe8e50682bdd5e6cc
Darwin Facelo <darwinfacelo@yahoo.com>
2023-03-06 12:11:18 -0800

    css for styling

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,12 +2,15 @@ import Navbar from "./components/navbar/Navbar";
 import Secondary from "./components/Secondary/Secondary"
 import {MetubeProvider} from './context/MetubeContext';
 import Video  from "./components/video_component/Video";
+import Details from "./components/details/Details";
 function App() {
+
   return (
       <MetubeProvider>
         <Navbar/>
         <Video />
         <Secondary />
+        <Details />
       </MetubeProvider>
   );
 }
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -6,6 +6,8 @@ import './index.css'
 import App from './App';
 import {MetubeProvider} from './context/MetubeContext';
 import './components/video_component/video.css'
+import './components/details/details.css'
+import './components/Secondary/secondary.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
