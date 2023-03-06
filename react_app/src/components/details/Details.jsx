d66dadcf09a99fc7cddf0115d938b728cb0b6920
Darwin Facelo <darwinfacelo@yahoo.com>
2023-03-06 12:10:49 -0800

    css for video

---
+++ react_app/src/components/details/Details.jsx
@@ -0,0 +1,9 @@
+const Details = () => {
+  return (
+    <div className="detail">
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default Details;
--- react_app/src/components/video_component/Video.jsx
+++ react_app/src/components/video_component/Video.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactPlayer from "react-player";
 const Video = () => {
   return (
-    
-        <div className="main-video">
-          <div className="video">
-          <ReactPlayer url='https://www.youtube.com/watch?v=_0XUN8NXOCY' controls='false' width='100%' height='100%'/>
-          </div>
-        </div>
+    <div className="main-video">
+      <div className="video">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=_0XUN8NXOCY"
+          controls="false"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </div>
   );
 };
 
