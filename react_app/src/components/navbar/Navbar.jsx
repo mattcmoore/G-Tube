fced5b28d3ce0c7717e872b123282fa3b946a7ba
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-03-08 09:42:05 -0600

    Finished cleaning up styling with all of the components added

--- react_app/src/components/navbar/Navbar.jsx
+++ react_app/src/components/navbar/Navbar.jsx
@@ -88,7 +88,7 @@ function Navbar() {
         <Tippy content='Search with your voice' arrow={false}>
           <div className="rounded-button" style={{'margin-left': '7px',
                                                   'background-color' : 'rgba(128, 128, 128, 0.5)',
-                                                  'margin-top': '7px',
+                                                  'margin-top': '4px',
                                                   "border-radius" : '0px',
                                                   'margin-right': '20px'}}>
               <FontAwesomeIcon className='icon' icon={faMicrophone} style={{'margin-top': '3px', 'width': '40px'}} />
@@ -106,7 +106,7 @@ function Navbar() {
         
         <div className="small-container" style={{'width': '200px'}}>
         <Tippy content='Search' arrow={false}>
-          <div onClick={toggleSearch} className="rounded-button" style={{'margin-left': '50px',
+          <div onClick={toggleSearch} className="rounded-button" style={{'margin-left': '60px',
                                                   'margin-top': '7px', 
                                                   'margin-right': '8px',
                                                   }}>
@@ -134,31 +134,28 @@ function Navbar() {
         <Tippy content='Search with your voice' arrow={false}>
         <div className="rounded-button" style={{'margin-left': '7px',
                                                 'background-color' : 'rgba(128, 128, 128, 0.5)',
-                                                'margin-top': '7px',}}>
-            <FontAwesomeIcon className='icon' icon={faMicrophone} style={{'margin-top': '3px', 'width': '40px'}} />
+                                                'margin-top': '4px',}}>
+            <FontAwesomeIcon className='icon' icon={faMicrophone} style={{'margin-top': '2px', 'width': '40px'}} />
         </div>
         </Tippy>
         </div>
       )}
-      
 
-
-      
       <div className='search-container'>
           <Tippy content='Settings' disabled={isDropdown} arrow={false}>
-          <div className="rounded-button" onClick={toggleDropdown}>           
+          <div className="rounded-button" onClick={toggleDropdown} style={{'margin-top': '8px'}}>           
             <FontAwesomeIcon className='icon' icon={faEllipsisV} />
           </div>
           </Tippy>
             {isDropdown && (
               <div className="dropdown-menu">
-                <ul>
-                  <li><FontAwesomeIcon className='icon' icon={faShield}/><a >Your data in Tutuge</a></li>
+                <ul style={{'margin-top': '0', 'margin-bottom': '0'}}>
+                  <li ><FontAwesomeIcon className='icon' icon={faShield}/><a >Your data in Tutuge</a></li>
                   <hr className="dropdown-divider" />
-                  <li onClick={openTheme}><FontAwesomeIcon className='icon' icon={theme.apperance === 'Dark Theme' ? faMoon : faSun}/><a>Appearance: {theme.apperance}</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"7px"}}/></li>
-                  <li><FontAwesomeIcon className='icon' icon={faLanguage}/><a >Language: English</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"55px"}} /></li>
-                  <li onClick={openRestrict}><FontAwesomeIcon className='icon' icon={faShieldAlt}/><a >Restricted Mode: {restricted.restrict}</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"39px"}}/></li>
-                  <li><FontAwesomeIcon className='icon' icon={faGlobe}/><a >Location: {location.country}</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"22px"}}/></li>
+                  <li onClick={openTheme}><FontAwesomeIcon className='icon' icon={theme.apperance === 'Dark Theme' ? faMoon : faSun}/><a>Appearance: {theme.apperance}</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"8px"}}/></li>
+                  <li><FontAwesomeIcon className='icon' icon={faLanguage}/><a >Language: English</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"57px"}} /></li>
+                  <li onClick={openRestrict}><FontAwesomeIcon className='icon' icon={faShieldAlt}/><a >Restricted Mode: {restricted.restrict}</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"41px"}}/></li>
+                  <li><FontAwesomeIcon className='icon' icon={faGlobe}/><a >Location: {location.country}</a><FontAwesomeIcon className='icon' icon={faArrowAltCircleRight} style={{'margin-left':"24px"}}/></li>
                   <li><FontAwesomeIcon className='icon' icon={faKeyboard}/><a >Keyboard shortcuts</a></li>
                   <hr className="dropdown-divider" />
                   <li><FontAwesomeIcon className='icon' icon={faGear}/><a >Settings</a></li>
@@ -171,10 +168,10 @@ function Navbar() {
             {theme.open && (
               <div className="dropdown-menu">
                 <div className="mic-container" >
-                 <div onClick={toggleDropdown} className="rounded-button" style={{"margin-top": '11px', 'margin-right': '10px', 'margin-left' : '7px'}}>           
+                 <div onClick={toggleDropdown} className="rounded-button"style={{"margin-top": '0', 'margin-right': '10px', 'margin-left' : '7px', 'margin-bottom': '7px'}}>          
                 <FontAwesomeIcon className='icon' icon={faArrowLeft} />
                 </div>
-                <h3 style={{'color': 'white'}}>Appearance</h3>
+                <h5 style={{'color': 'white', 'margin-top': '8px', 'margin-botton': '20px'}}>Appearance</h5>
                 </div>
                 <hr className="dropdown-divider" style={{'margin-top': '0'}}/>
                 <li onClick={setDark} ><FontAwesomeIcon className='icon' icon={theme.apperance === 'Dark Theme' ? faCheck: faMoon}/><a >Dark Theme</a></li>
@@ -184,25 +181,27 @@ function Navbar() {
             {restricted.open && (
               <div className="dropdown-menu">
               <div className="mic-container" >
-               <div onClick={toggleDropdown} className="rounded-button" style={{"margin-top": '11px', 'margin-right': '10px', 'margin-left' : '7px'}}>           
+               <div onClick={toggleDropdown} className="rounded-button" style={{"margin-top": '0', 'margin-right': '10px', 'margin-left' : '7px', 'margin-bottom': '7px'}}>           
               <FontAwesomeIcon className='icon' icon={faArrowLeft} />
               </div>
-              <h3 style={{'color': 'white'}}>Restricted Mode</h3>
+              <h5 style={{'color': 'white', 'margin-top': '8px', 'margin-botton': '20px'}}>Restricted Mode</h5>
               </div>
               <hr className="dropdown-divider" style={{'margin-top': '0'}}/>
               <p style={{'margin-left': '15px' ,'margin-bottom': '0', 'color' : 'white'}}>This helps hid potentially mature videos.</p>
               <p style={{'margin-left': '15px' ,'margin-top': '0', 'color' : 'white'}}>No filter is 100% accurate.</p>
               <p style={{'margin-left': '15px', 'color' : 'white'}}>This setting only applies to this browser.</p>
               <div className="mic-container">
-              <h3 style={{'margin-left': '15px', 'color': '#F7F7F7', 'font-weight': '600'}}>Activate Restricted Mode</h3>
-              <input type="range" min='0' max='1' step='1' onChange={toggleRestrict} value={restricted.restrict === 'Off' ? '0' : '1'} style={{'width': '30px', 'height' : '20px', 'margin-left': '20px', 'margin-top' : '22px'}}/>
+              <h3 style={{'margin-left': '15px', 'color': '#F7F7F7', 'font-weight': '600', 'padding-top': '15px'}}>Activate Restricted Mode</h3>
+              <input type="range" min='0' max='1' step='1' onChange={toggleRestrict} value={restricted.restrict === 'Off' ? '0' : '1'} style={{'width': '30px', 'height' : '20px', 'margin-left': '10px', 'margin-top' : '22px', 'margin-right': '8px'}}/>
               </div>
             </div>
             )}
-        <div className="oval-button">
+        <div className="oval-button" style={{'margin-top': '12px'}}>
           <FontAwesomeIcon icon={faUser} style={{'margin-right': '5px',
-                                                'color' : '#1e90ff'}}/>
-          <p style={{'color' : '#1e90ff'}}>Sign In</p>
+                                                'color' : '#1e90ff',
+                                                }}/>
+          <p style={{'color' : '#1e90ff',
+                      'margin-top': '15px'}}>Sign In</p>
         </div>
       </div>
     </div>
