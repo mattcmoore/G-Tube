a8948ae43ebaa9565885bb8dbbc822c8348bbf71
Dany Briceno <danybriceno@Danys-Air.lan>
2023-03-10 13:33:37 -0600

    Queue

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
 import Secondary from "./components/Secondary/Secondary"
 import {MetubeProvider} from './context/MetubeContext';
+import Queue from "./components/QueueComp/Queue";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
       <MetubeProvider>
-        <Navbar/>
-        <Secondary />
+            <Queue />
+        {/* <Navbar/>
+        <Secondary /> */}
       </MetubeProvider>
   );
 }
