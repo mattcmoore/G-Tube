4893daa5e04cc4bb492eafe0cac5ce6604787460
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-03-11 11:12:37 -0600

    Added some stuff to the server

--- react_app/server/server.js
+++ react_app/server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { Pool } = require("pg");
 const app = express();
 const dotenv = require("dotenv");
+const path = require('path')
 dotenv.config();
 
 //starting this don't not to forget to migrate your table.sql
@@ -15,6 +16,9 @@ app.use(express.json());
 // Middleware to enable CORS
 app.use(cors());
 
+
+// app.use(express.static(path.join(__dirname, '../build')))
+
 // Create a connection pool to the database
 
 // const pool = new Pool({
@@ -25,8 +29,8 @@ app.use(cors());
 //   database: "youtube",
 // });
 
-// const connectionString = process.env.DATABASE_URL
-const connectionString = 'postgresql://fatbo@localhost:5432/youtube'
+const connectionString = process.env.DATABASE_URL
+// const connectionString = 'postgresql://fatbo@localhost:5432/youtube'
 
 
 const pool = new Pool({
@@ -34,6 +38,15 @@ const pool = new Pool({
 })
 
 const PORT = process.env.PORT || 3001;
+
+// app.get('/', (req,res)=>{
+//   try {
+//     res.sendFile(path.join(__dirname, '../public/index.html'))
+    
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })
 
 // API endpoint to retrieve a simple JSON object
 app.get("/testApi", async (req, res) => {
